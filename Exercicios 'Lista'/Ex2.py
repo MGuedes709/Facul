b539c6ac3df5797ef16974a36c80470a9e9134fe
Marco <128433782+MGuedes709@users.noreply.github.com>
2024-03-20 17:28:58 -0300

    Delete Exercicios 'Lista' directory

--- Exercicios 'Lista'/Ex2.py
+++
@@ -1,9 +0,0 @@
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   
-def filtrar_pares(lista):
-    for i in numeros:
-        if i % 2!=0:
-            numeros.remove(i)
-    return
-
-pares = filtrar_pares(numeros)
-print("Números pares:", numeros)
